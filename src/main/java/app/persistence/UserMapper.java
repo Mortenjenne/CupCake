9a287feab61f4ed0e0054ce0755cce9cce98168c
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-10-22 00:42:09 +0200

    UserMapper: Added body to updateUserBalance(). Note: This methods set the balance of the user - we mig need to refactor so that the amount is added instead

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -131,8 +131,24 @@ public class UserMapper
         return false;
     }
 
-    public void updateUserBalance(int userId, double amount)
+    // TODO where do we calculate new balance?
+    // Amount is set, not added in this method - we might need to refactor
+    public void updateUserBalance(int userId, double amount) throws DatabaseException
     {
+        String sql = "UPDATE users SET balance = ? WHERE user_id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setDouble(1, amount);
+            ps.setInt(2, userId);
+
+            ResultSet rs = ps.executeQuery();
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Beløbet " + amount + ",- blev ikke tilføjet til bruger med id: " + userId);
+        }
     }
 
     public User login(String email, String password) throws DatabaseException
