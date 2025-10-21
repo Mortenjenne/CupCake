ac12311c9dc505625209b3e1de959a510f0da7fb
Morten Jensen <mo_ten@hotmail.com>
2025-10-21 16:16:35 +0200

    implemented routes for createuser.html and login.html

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,5 +1,40 @@
 package app.controllers;
 
+import app.services.UserService;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+
 public class UserController
 {
+    private UserService userService;
+
+    public UserController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    public void registerRoutes(Javalin app)
+    {
+        app.get("/login", ctx -> showLoginPage(ctx));
+        app.get("/create-user", ctx -> showCreateUserPage(ctx));
+
+        app.post("/create-user", ctx -> handleCreateUser(ctx));
+        app.post("/login", ctx -> handleUserLogin(ctx));
+    }
+
+    private void handleUserLogin(Context ctx)
+    {
+    }
+
+    private void handleCreateUser(Context ctx)
+    {
+    }
+
+    private void showCreateUserPage(Context ctx)
+    { ctx.render("createuser"); }
+
+    private void showLoginPage(Context ctx)
+    { ctx.render("/login"); }
+
 }
--- src/main/java/app/services/UserServiceImpl.java
+++ src/main/java/app/services/UserServiceImpl.java
@@ -1,5 +1,13 @@
 package app.services;
 
+import app.persistence.UserMapper;
+
 public class UserServiceImpl implements UserService
 {
+    private UserMapper userMapper;
+
+    public UserServiceImpl(UserMapper userMapper)
+    {
+        this.userMapper = userMapper;
+    }
 }
