547e77bbf012a1a4ecbf2ff05ad11783731faa88
Obliwobbi <tobiashartzberg@gmail.com>
2025-10-21 19:06:09 +0200

    Added methods for getAllToppings and getToppingById. Needed for making the index page to order cupcakes.

--- src/main/java/app/persistence/ToppingMapper.java
+++ src/main/java/app/persistence/ToppingMapper.java
@@ -1,28 +1,68 @@
 package app.persistence;
 
 import app.entities.Topping;
+import app.exceptions.DatabaseException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToppingMapper
 {
-    private ConnectionPool connectionPool;
+    private final ConnectionPool connectionPool;
 
     public ToppingMapper(ConnectionPool connectionPool)
     {
         this.connectionPool = connectionPool;
     }
 
-    public List<Topping> getAllToppings()
+    public List<Topping> getAllToppings() throws DatabaseException
     {
         List<Topping> toppingList = new ArrayList<>();
-
+        String sql = "SELECT * FROM toppings";
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery())
+        {
+            while(rs.next())
+            {
+                Topping t = new Topping(
+                rs.getInt("topping_id"),
+                rs.getString("flavour"),
+                rs.getDouble("price")
+                );
+                toppingList.add(t);
+            }
+        } catch (SQLException e)
+        {
+            throw new DatabaseException("Database error while fetching toppings: " + e);
+        }
         return toppingList;
     }
 
-    public Topping getToppingById(int bottomId)
+    public Topping getToppingById(int bottomId) throws DatabaseException
     {
-        return null;
+        String sql = "SELECT * FROM toppings WHERE topping_id = ?";
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, bottomId);
+            try(ResultSet rs = ps.executeQuery())
+            {
+                if(rs.next())
+                {
+                    return new Topping(rs.getInt("topping_id"),
+                            rs.getString("flavour"),
+                            rs.getDouble("price"));
+                }
+                return null;
+            }
+        } catch (SQLException e)
+        {
+            throw new DatabaseException("Database error while fetching toppings: " + e);
+        }
     }
 
     public Topping createTopping(Topping topping)
