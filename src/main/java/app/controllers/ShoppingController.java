68e0588775fe949ae2716422169d79e392e4cca4
Obliwobbi <tobiashartzberg@gmail.com>
2025-10-21 21:43:42 +0200

    ShoppingController: Added showIndex HasMap model to index page.

--- src/main/java/app/controllers/ShoppingController.java
+++ src/main/java/app/controllers/ShoppingController.java
@@ -1,12 +1,15 @@
 package app.controllers;
 
+import app.entities.Bottom;
 import app.entities.ShoppingCart;
 import app.entities.Topping;
+import app.exceptions.DatabaseException;
 import app.services.ShoppingService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,11 +25,32 @@ public class ShoppingController
 
     public void addRoutes(Javalin app)
     {
-        app.get("/", ctx -> showIndex());
+        app.get("/", this::showIndex);
+        app.post("/cart/add", this::addToCart);
+        //TODO: post på remove line og clear cart?
     }
 
-    private void showIndex() {
-        List<Topping> toppingList = shoppingService.getAllBottoms();
+    private void showIndex(Context ctx) throws DatabaseException
+    {
+        var model = new HashMap<String, Object>();
+        model.put("bottoms",shoppingService.getAllBottoms());
+        model.put("toppings",shoppingService.getAllToppings());
+        model.put("cart",getOrCreateCart(ctx));
+        ctx.render("index.html");
+    }
+
+    private ShoppingCart getOrCreateCart(Context ctx)
+    {
+        ShoppingCart cart = ctx.sessionAttribute("CART");
+        if(cart == null)
+        {
+            cart = new ShoppingCart();
+            ctx.sessionAttribute("CART", cart);
+        }
+        return cart;
+    }
+
+    private void addToCart(Context context) {
     }
 
 }
